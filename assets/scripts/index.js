252a7c8fc9ef631ab4e10f7fc0b90880ec09af77
nishkixs <nishon.radhakrishnan@gmail.com>
2016-11-28 21:38:00 -0500

    working sign in

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,4 +4,46 @@
 // var example = require('./example');
 
 // use require without a reference to ensure a file is bundled
-require('./example');
+
+
+
+
+
+$(() => {
+  $('#sign-in-nav').on('click', function() {
+  $('#signInModal').modal('show');
+ });
+ $('#sign-up-nav').on('click', function() {
+ $('#signUpModal').modal('show');
+ });
+ $('#btnLogin').on('click', function(){
+    //  let firebase = require('firebase');
+     let config = {
+       apiKey: "AIzaSyBkmw-vbMTDNGJss3bDQrRTaJCslMCP65s",
+       authDomain: "bettor-half-70e6d.firebaseapp.com",
+       databaseURL: "https://bettor-half-70e6d.firebaseio.com",
+       storageBucket: "bettor-half-70e6d.appspot.com",
+       messagingSenderId: "495009639670"
+     };
+     firebase.initializeApp(config);
+
+     const txtEmail = document.getElementById('txtEmail');
+     const txtPassword = document.getElementById('txtPassword');
+     const btnLogin = document.getElementById('btnLogin');
+     // const btnSignUp = document.getElementById('btnLogout');
+
+     btnLogin.addEventListener('click', e => {
+
+           const email = txtEmail.value;
+           const pass = txtPassword.value;
+           const auth = firebase.auth();
+           // Sign in
+           const promise = auth.signInWithEmailAndPassword(email, pass);
+           promise.catch(e => console.log(e.message));
+       });
+
+
+
+ });
+
+});
